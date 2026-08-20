74a381c8245a69ef46aaade5e29f21fb13248b1d
Ardavamik <ardavamik@gmail.com>
2026-08-20 17:05:46 +0300

    Small changes3

--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'productionfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 
